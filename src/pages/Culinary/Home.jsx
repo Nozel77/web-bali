e710666d130e4803419b0804072bf4762e70a50d
MESKING125 <marlen7891011@gmail.com>
2023-10-10 13:56:11 +0700

    [MES] Make new branches

---
+++ src/pages/Culinary/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../../components/culinary/Header'
+import Recipes from '../../components/culinary/Recipes'
+import Navbar from '../../components/Navbar'
+
+const Home = () => {
+  return (
+    <main className='w-full flex flex-col'>
+      <Header
+        title={
+          <p className='text-white'>
+            Beragam Kuliner Khas 
+            <br /> Bali!
+          </p>
+        }
+        type='home'
+      />
+
+      <section id="recipes" className='md:max-w-[1440px] mx-auto px-4 md:px-20'>
+        <Recipes />
+      </section>
+    </main>
+  )
+}
+
+export default Home
