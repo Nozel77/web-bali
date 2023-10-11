2e436b4e788a1918a5bf11153b483f673b93f90a
Nozell77 <rafisidqi999@gmail.com>
2023-10-11 10:25:13 +0700

    [RS] add animation gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,28 +1,88 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
 
 const Gallery = () => {
-return (
-    <div id='gallery' className='max-w-[1140px] m-auto w-full px-4 py-16'>
-        <h2 className='text-center text-gray-700 p-4'>Gallery</h2>
-        <div className='flex flex-col gap-4 lg:flex-row'>
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1536,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+            {
+                breakpoint: 1280,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 1
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+  return (
+    <div id="gallery" className="max-w-[1140px] mt-60 m-auto w-full px-4 py-16">
+      <h2 className="text-center text-gray-700 p-4">Gallery</h2>
+      <div>
+          <div className="flex flex-col gap-4 ">
+            <Slider {...settings}>
             <div>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1615571022219-eb45cf7faa9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" />
+              <img className="w-full h-full object-cover p-2" src="https://images.unsplash.com/photo-1615571022219-eb45cf7faa9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" />
             </div>
             <div>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1594398901394-4e34939a4fd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="/" />
+              <img className="w-full h-full object-cover p-2" src="https://images.unsplash.com/photo-1594398901394-4e34939a4fd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="/" />
             </div>
             <div>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="/" />
+              <img className="w-full h-full object-cover p-2" src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="/" />
             </div>
             <div>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1578530332818-6ba472e67b9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80" alt="/" />
+              <img className="w-full h-full object-cover p-2" src="https://images.unsplash.com/photo-1578530332818-6ba472e67b9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80" alt="/" />
             </div>
             <div>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1613553507747-5f8d62ad5904?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="/" />
+              <img className="w-full h-full object-cover p-2" src="https://images.unsplash.com/photo-1613553507747-5f8d62ad5904?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="/" />
             </div>
-        </div>
+            </Slider>
+          </div>
+        
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/pages/LandingPage/Landingpage.jsx
+++ src/pages/LandingPage/Landingpage.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Hero from '../../components/Hero'
-import Activities from '../../components/Activity'
-import Gallery from '../../components/Gallery'
-import Footer from '../../components/Footer'
-import Course from '../../components/Course'
-
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Hero from '../../components/Hero';
+import Activities from '../../components/Activity';
+import Gallery from '../../components/Gallery';
+import Footer from '../../components/Footer';
 
 const Landingpage = () => {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Activities/>
-    {/* <Course/> */}
-    <Gallery/>
-    <Footer/>
+      <Navbar />
+      <Hero />
+      <Activities />
+      <Gallery />
+      <Footer />
+      {/* <Swiper/> */}
     </>
-  )
-}
+  );
+};
 
-export default Landingpage
+export default Landingpage;
