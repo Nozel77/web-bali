b84da9c839027b432a0fab0dd41d442d6ec8964f
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2023-10-13 15:33:31 +0700

    [DNA] slight style change

--- src/pages/Economy/Economy.jsx
+++ src/pages/Economy/Economy.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Ekonomi from '../../components/Ekonomi'
+import Footer from '../../components/Footer'
 
 const Economy = () => {
   return (
     <>
     <Navbar/>
     <Ekonomi/>
+    <Footer/>
     </>
     
   )
